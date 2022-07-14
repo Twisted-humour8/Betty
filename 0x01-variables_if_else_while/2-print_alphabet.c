f8c580bec97690ac28762ae6d0c6f7713396d783
Twisted-humour8 <sashenmaharaj1@gmail.com>
2022-07-14 07:56:07 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - entry point
+ *
+ * return: always 0 (success)
+*/
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
