2b411c39a09da47efc5aea288a9029bd5bba9d37
Twisted-humour8 <sashenmaharaj1@gmail.com>
2022-07-14 08:18:15 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+*/
+*main - entry point
+*
+*return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printtf("\n");
+
+	return (0);
+}
